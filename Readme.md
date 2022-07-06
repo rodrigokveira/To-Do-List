567569e295534794ba5cf0966b956fec9e34a4e2
rodrigokveira <rodrigoac2007@gmail.com>
2022-07-06 20:19:29 -0300

    Melhorias e adição do Readme.md

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# To-Do List
+
+Desenvolvido na aula 5 de Introdução ao JavaScript do Bootcamp da TQI Fullstack Developer pela DIO.
+
+>Referência: https://github.com/stebsnusch/basecamp-javascript/tree/main/introducao-ao-javascript/to-do%20list
+
+Além de fazer o proposto no desafio, também foram implementados:
+* Texto inicial do `input` é removido ao ganhar o foco e retornado caso seu valor permaneça vazio ao perder o foco. Sua cor também varia em relação ao seu foco e valor.
+* Colocada uma verificação do texto do `input` ao clicar no botão. Caso o valor seja o inicial ou vazio, não será adicionado à lista.
+* O texto do `input` retorna ao valor inicial depois de ser adicionado à lista.
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,21 +1,44 @@
 var buttonAdd = document.getElementById("buttonAdd");
 var lista = document.getElementById("lista");
 var inputTask = document.getElementById("inputTask");
+var initialText = inputTask.value;
 
 function addItem() {
-    const item = document.createElement("div");     // cria uma nova div
-    item.setAttribute('class','item');              // define a classe da div como item
-    const checkbox = document.createElement("input"); // cria um novo input
-    checkbox.setAttribute('type','checkbox');       // define o tipo de input para checkbox
-    // var text = inputTask.value;
-    const para = document.createElement("p");       // cria um novo parágrafo
-    // const node = document.createTextNode(text);
-    // para.appendChild(node);
-    // para.innerText = text;
-    para.innerText = inputTask.value;               // coloca o value do input no texto do parágrafo
-    item.appendChild(checkbox);                     // adiciona o checkbox à div criada
-    item.appendChild(para);                         // adiciona o parágrafo à div criada
-    lista.appendChild(item);                        // adiciona a div criada à div lista
+    if (inputTask.value != initialText && inputTask.value != ""){
+        const item = document.createElement("div");     // cria uma nova div
+        item.setAttribute('class','item');              // define a classe da div como item
+        const checkbox = document.createElement("input"); // cria um novo input
+        checkbox.setAttribute('type','checkbox');       // define o tipo de input para checkbox
+        // var text = inputTask.value;
+        const para = document.createElement("p");       // cria um novo parágrafo
+        // const node = document.createTextNode(text);
+        // para.appendChild(node);
+        // para.innerText = text;
+        para.innerText = inputTask.value;               // coloca o value do input no texto do parágrafo
+        item.appendChild(checkbox);                     // adiciona o checkbox à div criada
+        item.appendChild(para);                         // adiciona o parágrafo à div criada
+        lista.appendChild(item);                        // adiciona a div criada à div lista
+        inputTask.value = initialText;
+        inputTask.style.color = "rgb(165, 165, 165)";
+    }
+}
+
+function inputTextInitial() {
+    if (inputTask.value == initialText) {
+        inputTask.value = "";
+        inputTask.style.color = "black";
+    }
+}
+
+function inputTextCheck() {
+    if (inputTask.value == "") {
+        inputTask.value = initialText;
+        inputTask.style.color = "rgb(165, 165, 165)";
+    } else {
+        inputTask.style.color = "black";
+    }
 }
 
 buttonAdd.addEventListener("click", addItem);
+inputTask.addEventListener("focus", inputTextInitial);
+inputTask.addEventListener("focusout", inputTextCheck);
